9fed9a3e0b44c72e6cc9bba6f3b39de637c7d018
Devin Murnan <devin.murnan@wwt.com>
2022-03-10 14:59:22 -0600

    1 - Initial build for lemon juicer - DM

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import logo from './logo.svg';
+import lemon from './lemon.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+
 
 function App() {
+  let [count, setCount, message] = useState(0);
+  const incrementCount = () => { 
+    setCount(count + 1);
+   }
+   if (count === 0){ message = "Welcome! Click the lemon to start juicing!"}
+   if (count === 18){ message = "You've juiced enough lemons for a gallon of lemonade!"}
+   
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
+        <button style={{outline: 0}} onClick={incrementCount}><img src={lemon} className="App-logo" alt="logo" /></button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You have juiced {count} lemons.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{message}</p>
       </header>
     </div>
   );
