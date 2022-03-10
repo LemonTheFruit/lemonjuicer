f3e9492a09b9a496c77203b50fc09d618fd00d2a
Devin Murnan <devin.murnan@wwt.com>
2022-03-10 16:36:54 -0600

    4 - Minor tweak to main page, add logos - DM

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import React, { useState } from 'react';
 
 
 function App() {
-  let [count, setCount, message] = useState(0);
+  let message = ""
+  const [count, setCount] = useState(0);
+  // let [message, setMessage] = useState("")
   const incrementCount = () => { 
     setCount(count + 1);
    }
-   if (count === 0){ message = "Welcome! Click the lemon to start juicing!"}
+   if (count === 0){ message = "Welcome! Click the lemon to begin juicing." }
    if (count === 18){ message = "You've juiced enough lemons for a gallon of lemonade!"}
    
   return (
     <div className="App">
       <header className="App-header">
         
-        <button style={{outline: 0}} onClick={incrementCount}><img src={lemon} className="App-logo" alt="logo" /></button>
+        <button onClick={incrementCount}><img src={lemon} className="App-logo" alt="logo" /></button>
         <p>
           You have juiced {count} lemons.
         </p>
